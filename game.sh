580c6d43d76a2fb277ad7a98a12a56bb9610a4f2
Licons Chou <liconschou@gmail.com>
2025-10-04 20:41:27 +0700

    Update Game

---
+++ game.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+yay -Syu --noconfirm
+
+yay -S --noconfirm steam
+yay -S --noconfirm protonup-qt gamemode mangohud lib32-mangohud dxvk-bin vkd3d-proton-bin
+mkdir -p ~/.steam/root/compatibilitytools.d
+protonup-qt --install
+
+# thêm vào Properties của GAME: gamemoderun mangohud %command%
+
+echo "Hoàn tất cài đặt!"
