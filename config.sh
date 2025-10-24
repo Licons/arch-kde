2f479b074464d687eb6bdfe1eba3bb5d955a4052
Qua, Chau Ngoc <qua.cn@tripota.com.vn>
2025-10-24 14:37:55 +0700

    Update

--- config.sh
+++ config.sh
@@ -20,8 +20,10 @@ if status is-interactive
 end
 
 set -g fish_greeting
-set -x DOTNET_CLI_TELEMETRY_OPTOUT 1
 set -gx PATH \$PATH /home/$username/.dotnet/tools
+
+set -x DOTNET_CLI_TELEMETRY_OPTOUT 1
+set -x LC_ALL C.UTF-8
 EOF
 
 mkdir -p ~/.config/fish
